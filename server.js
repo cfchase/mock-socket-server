aba213fe0234035fbdaf46224aced521fcbba2b9
Chris Chase <cchase@redhat.com>
2019-02-12 14:28:52 -0500

    Added websockets and client requests.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const Hapi = require("hapi");
 const HapiPino = require("hapi-pino");
 const Inert = require("inert");
+const Nes = require('nes');
+
 
 const PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 const IP = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
@@ -17,8 +19,43 @@ server.app = require("./utils/settings/app");
 const init = async () => {
   await server.register(Inert);
   await server.register(HapiPino);
+  await server.register(Nes);
+
+  // server.route({
+  //   method: 'GET',
+  //   path: '/h',
+  //   config: {
+  //     id: 'hello',
+  //     handler: (request, h) => {
+  //
+  //       return {"world": request.payload};
+  //     }
+  //   }
+  // });
 
   require("./routes/api/status")(server);
+
+  server.route({
+    method: "GET",
+    path: "/{path*}",
+    handler: {
+      directory: {
+        path: "ui/build"
+      }
+    }
+  });
+
+  server.ext("onPreResponse", (request, h) => {
+    const response = request.response;
+    const isNotApi = !/^\/api/g.exec(request.url.path);
+    if (isNotApi && response.isBoom &&
+      response.output.statusCode === 404) {
+      return h.file("ui/build/index.html");
+    }
+
+    return h.continue;
+  });
+
 
   await server.start();
   console.log(`Server running at: ${server.info.uri}`);
